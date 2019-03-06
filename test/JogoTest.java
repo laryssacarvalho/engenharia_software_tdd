041b50241359b40bed1edaad74aa14252aca7aaf
danielb05 <dan.b.v.r05@gmail.com>
2019-03-05 22:15:20 -0300

    Teste todoJogoTemDescricao() - Daniel

--- test/JogoTest.java
+++ test/JogoTest.java
@@ -1,10 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import builder.CriadorDeJogo;
 import dominio.Jogo;
 import dominio.Participante;
+import junit.framework.Assert;
 
 class JogoTest {	
 	
@@ -17,6 +21,16 @@ class JogoTest {
 		.constroi();
 		
 		assertEquals(2, jogo.getResultados().size());
+	}
+	
+	@Test
+	void todoJogoTemDescricao() {
+		Jogo jogo = new CriadorDeJogo()
+		.para("Teste Jogo")
+		.constroi();		
+		
+	   assertFalse(jogo.getDescricao() == null);
+	   assertFalse(jogo.getDescricao().isEmpty());
 	}
 
 }
